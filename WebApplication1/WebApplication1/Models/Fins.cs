18c8599f11c0be52002b9035a927daed60cde2e1
Barrett <Kristen.Barrett@parivedasolutions.com>
2017-06-20 15:00:37 -0500

    db structure and permissions

---
+++ WebApplication1/WebApplication1/Models/Fins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindYourPod.Models
+{
+    public class Fin
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Office { get; set; }
+        public string Steam { get; set; }
+        public string Battle { get; set; }
+        public string League { get; set; }
+        public string Xbox { get; set; }
+        public string Psn { get; set; }
+        public string Nintendo { get; set; }
+    }
+}
