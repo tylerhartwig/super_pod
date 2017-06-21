e7eb3b9d8589feca9ff756df634b7198502248bf
Carlos Cardin <carloscardin@CCARDIN-MBP-DAL.local.tld>
2017-06-21 14:01:51 -0500

    fixed developer profile layout issue

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var addbtn = document.getElementById('add-btn');
+var cancelbtn = document.getElementById('cancel-btn');
+var submitbtn = document.getElementById('submit-btn');
+
+var modal = document.getElementById('modal');
 
 addbtn.addEventListener("click", function(){
-	var modal = document.getElementById('modal');
 	modal.classList.toggle("active");
 	modal.calssList.toggle("scrolling");
 })
+
+cancelbtn.addEventListener("click", function(){
+	modal.classList.toggle("active");
+})
+
+submitbtn.addEventListener("click", function(){
+	//function 
+})
+
