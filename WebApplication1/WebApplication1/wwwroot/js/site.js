659ac97e0679d71a0afc993c1fb634d707c581bd
Barrett <Kristen.Barrett@parivedasolutions.com>
2017-06-22 10:38:11 -0500

    working front page w/o text + db

--- WebApplication1/WebApplication1/wwwroot/js/site.js
+++ WebApplication1/WebApplication1/wwwroot/js/site.js
@@ -1,1 +1,32 @@
-﻿// Write your Javascript code.
+﻿var addbtn = document.getElementById('add-btn');
+var cancelbtn = document.getElementById('cancel-btn');
+var submitbtn = document.getElementById('submit-btn');
+
+var modal = document.getElementById('modal');
+
+$(document).ready(function () {
+    console.log("TEST");
+    $('.ui.dropdown').dropdown();
+})
+
+addbtn.addEventListener("click", function () {
+    modal.classList.toggle("active");
+    modal.calssList.toggle("scrolling");
+})
+
+cancelbtn.addEventListener("click", function () {
+    modal.classList.toggle("active");
+})
+
+submitbtn.addEventListener("click", function () {
+    //function 
+})
+
+$('.dropdown').dropdown({
+    label: {
+        duration: 0,
+    },
+    debug: true,
+    performance: true,
+});
+
